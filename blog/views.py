9de8bd91b346c9e02013f403e06c511139adcef5
nicolascagna96 <nicolascagna@hotmail.com>
2022-10-30 23:06:40 +0000

    final deployment

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,8 @@ from django.contrib import messages
 from .models import Post
 from .models import Recipes
 
+# Class used in the 'I think therefore I blog' walkthrough
+
 
 class PostList(generic.ListView):
     model = Post
@@ -16,13 +18,7 @@ class PostList(generic.ListView):
     template_name = "index.html"
     paginate_by = 6
 
-
-def about_page(request):
-
-    """
-    Viewing the about page.
-    """
-    return render(request, 'about.html,', {})
+# Class used in the 'I think therefore I blog' walkthrough
 
 
 class PostDetail(View):
@@ -30,7 +26,7 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.order_by("created_on")
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -42,20 +38,21 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": False,
+                "liked": liked,
                 "comment_form": CommentForm()
             },
         )
 
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.order_by("created_on")
+        comments = post.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         comment_form = CommentForm(data=request.POST)
-
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
@@ -72,22 +69,26 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": True,
-                "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": comment_form,
+                "liked": liked
             },
         )
+
+# Class used in the 'I think therefore I blog' walkthrough
 
 
 class PostLike(View):
 
-    def post(self, request, slug):
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
-
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
+
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+# Submit database form
 
 
 def submit(request):
@@ -109,6 +110,8 @@ def submit(request):
         form = SubmitForm()
         return render(
             request, 'submit.html', {'form': form})
+
+# Contact database form
 
 
 def add_contact(request):
